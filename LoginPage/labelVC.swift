bd6545ad39cab1dfd811830140139161427e5268
Muralidhargithub <ot41975@umbc.edu>
2024-11-11 22:55:49 -0500

    version-1.2 Added Corner Radius. Height

---
+++ LoginPage/labelVC.swift
@@ -0,0 +1,20 @@
+//
+//  labelVC.swift
+//  LoginPage
+//
+//  Created by Muralidhar reddy Kakanuru on 11/11/24.
+//
+
+import UIKit
+
+class labelVC: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LoginPage/textvc.swift
@@ -0,0 +1,20 @@
+//
+//  textvc.swift
+//  LoginPage
+//
+//  Created by Muralidhar reddy Kakanuru on 11/11/24.
+//
+
+import UIKit
+
+class textvc: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
